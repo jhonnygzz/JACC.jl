7196b3da4a92c1435175522c2fbf24560f368962
jhonnygzz <108152933+jhonnygzz@users.noreply.github.com>
2024-11-14 12:47:59 -0500

    Update JACCCUDA.jl

--- ext/JACCCUDA/JACCCUDA.jl
+++ ext/JACCCUDA/JACCCUDA.jl
@@ -98,7 +98,7 @@ end
 function _parallel_for_cuda(N, f, x...)
     i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     i > N && return nothing
-    f(i, x...)
+        f(i, x...)
     return nothing
 end
 
